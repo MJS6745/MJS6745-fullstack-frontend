05f19656e42cb845f639c56d35ff96f5deb74a30
MJS6745 <Mark.Smets@libertymutual.com>
2017-10-16 15:59:11 -0400

    Tested layout with inclusion of test content
    Created dummy data to test the layout of the front end. In addition,
    created a layout for buttons that can be used for editing content.
    Buttons are not yet functional but will dynamically pull in data
    based on "data" attribute info

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,17 @@ const config = require('./config')
 
 $(() => {
   setAPIOrigin(location, config)
+  $('#exampleModal').on('show.bs.modal', function (event) {
+    const button = $(event.relatedTarget) // Button that triggered the modal
+    const workout = button.data('resourceid') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Add in
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    const modal = $(this)
+    modal.find('.modal-title').text('Altering workout ' + workout)
+    modal.find('.modal-body input').val(workout)
+    modal.find('.modal-body #message-text').text('Hey this is the text for workout ' + workout)
+  })
 })
 
 // use require with a reference to bundle the file and use it in this file
